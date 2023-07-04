6218da578a9fc53814b8d9dc011790f46f89e6a0
Gray007 <52993744+Gray007@users.noreply.github.com>
2023-07-04 18:10:22 +0200

    Cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const omdbRouter = require('./controllers/omdb')
 const config = require('./utils/config')
+
+app.use(cors());
 
 app.use('/api/omdb', omdbRouter)
 
